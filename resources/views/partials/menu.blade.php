0b5e5ee09c7bd80ab61de637eac656a78d926335
Romar Jabez <minecraftjabez@gmail.com>
2025-10-25 13:17:05 +0800

    tiwason pa nako ni

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,27 @@
+<div class="menu">
+
+    <a href="">
+        <span class="menu-item">
+            
+        </span>
+    </a>
+
+    <a href="">
+        <span class="menu-item">
+            
+        </span>
+    </a>
+
+    <a href="">
+        <span class="menu-item">
+            
+        </span>
+    </a>
+
+    <a href="">
+        <span class="menu-item">
+            
+        </span>
+    </a>
+
+</div>
