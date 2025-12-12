41b8ac5f7a365d5ce3e266c4977fbf7866de456f
Romar Jabez <minecraftjabez@gmail.com>
2025-12-12 17:42:25 +0800

    fixed menu UI

--- electron/preload.js
+++ electron/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
     minimize: () => ipcRenderer.send('window-minimize'),
     maximize: () => ipcRenderer.send('window-maximize'),
     close: () => ipcRenderer.send('window-close'),
+    showContextMenu: (position) => ipcRenderer.send('show-context-menu', position),
     onMaximizeChange: (callback) => ipcRenderer.on('maximize-change', (event, isMaximized) => callback(isMaximized))
 });
